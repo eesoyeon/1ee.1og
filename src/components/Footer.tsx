995a44eff026cc23b150445880c84348ecf63c1f
Soyeon Lee <pink731p@gmail.com>
2025-04-12 17:10:26 +0900

    Design: 헤더, 푸터

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full mt-20 py-6 text-center text-sm text-muted-foreground">
+      <p>1ee.1og - All rights reserved.</p>
+    </footer>
+  );
+}
